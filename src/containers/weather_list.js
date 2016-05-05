870f1a99e7641c0ff4cd6c32857fb577b5d88586
spawner999 <lore.ferrario@gmail.com>
2016-05-04 18:44:18 -0700

    make a reusable chart component, and add avgs

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,21 +1,25 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Sparklines, SparklinesLine} from 'react-sparklines';
-
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
     const temps = cityData.list.map(weather => weather.main.temp);
-
+    const pressures = cityData.list.map(weather => weather.main.pressure);
+    const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name}>
         <td>{name}</td>
         <td>
-          <Sparklines height={120} width={180} data={temps}>
-            <SparklinesLine color="red" />
-          </Sparklines>
+          <Chart data={temps} color="red" units="K"/>
+        </td>
+        <td>
+          <Chart data={pressures} color="green" units="hPa"/>
+        </td>
+        <td>
+          <Chart data={humidities} color="blue" units="%"/>
         </td>
       </tr>
     );
